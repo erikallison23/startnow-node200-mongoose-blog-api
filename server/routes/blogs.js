8876d22d1af4274d15dd9a86fbc5ae6d7875aa1d
Erik Allison <erikallison23@gmail.com>
2018-02-21 15:21:28 -0800

    lower case in blog.js

--- server/routes/blogs.js
+++ server/routes/blogs.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Blog = require('../models/Blog');
-const User = require("../models/User");
+const Blog = require('../models/blog');
+const User = require("../models/user");
 
 router.get('/', (req, res) => {
     Blog
