a180bf515c581f05d111aa5767f921d8b74dbd55
Erik Allison <erikallison23@gmail.com>
2018-02-21 14:39:40 -0800

    added uri

--- server/app.js
+++ server/app.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
-//mongoose.connect(uri);
+//mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
+mongoose.connect('mongodb://heroku_9jg6hmr6:m7q6nbiubto0fqumveim5npcvj@ds245518.mlab.com:45518/heroku_9jg6hmr6');
 mongoose.Promise = Promise;
 
 const app = express();
