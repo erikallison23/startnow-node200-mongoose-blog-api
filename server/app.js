e9b139c0d7e859e8109935e2bc87cbc583e8e2ea
Erik Allison <erikallison23@gmail.com>
2018-02-21 15:27:41 -0800

    added uri as variable

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const uri = process.env.MONGODB_URI || 'mongodb://heroku_9jg6hmr6:m7q6nbiubto0fqumveim5npcvj@ds245518.mlab.com:45518/heroku_9jg6hmr6'
 
 //mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
-mongoose.connect('mongodb://heroku_9jg6hmr6:m7q6nbiubto0fqumveim5npcvj@ds245518.mlab.com:45518/heroku_9jg6hmr6');
+mongoose.connect(uri);
 mongoose.Promise = Promise;
 
 const app = express();
